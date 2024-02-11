75e6a5519fa0d653c11b2dee694f0329937c9ece
Jonathan Walker <w@lker.dev>
2024-02-11 11:59:26 +0900

    tokyonight for telescope

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -7,7 +7,38 @@ local M = {
 
 function M.config()
   require("tokyonight").setup({
-    transparent = true
+    on_highlights = function(hl, c)
+    local prompt = "#2d3149"
+    hl.TelescopeNormal = {
+      bg = c.bg_dark,
+      fg = c.fg_dark,
+    }
+    hl.TelescopeBorder = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopePromptNormal = {
+      bg = prompt,
+    }
+    hl.TelescopePromptBorder = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopePromptTitle = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopePreviewTitle = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopeResultsTitle = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+  end,
+  transparent = true
+
   })
   vim.cmd[[colorscheme tokyonight]]
 end
